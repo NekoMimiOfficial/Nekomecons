fb436bfa4d1ee6c58d12860afda2660f016b623f
NekoMimi <nekomimiofficial@proton.me>
2025-07-24 19:00:38 +0300

    milestone 1: rebranding

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -41,7 +41,7 @@ dependencyResolutionManagement {
     }
 }
 
-rootProject.name = "Lawnicons"
+rootProject.name = "Nekomecons"
 
 enableFeaturePreview("STABLE_CONFIGURATION_CACHE")
 enableFeaturePreview("TYPESAFE_PROJECT_ACCESSORS")
